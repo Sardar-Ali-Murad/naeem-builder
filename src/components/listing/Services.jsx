7c7a0615a346d956816e8a5e928bc658c442dc6e
Sardar-Ali-Murad <mar389786@gmail.com>
2024-08-08 11:58:13 -0700

    changes

--- src/components/listing/Services.jsx
+++ src/components/listing/Services.jsx
@@ -10,8 +10,7 @@ const Services = () => {
           <img src="images/icon-1.png" alt="" />
           <h3>buy house</h3>
           <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Doloremque, incidunt.
+            Discover your dream home with our expert guidance and vast listings
           </p>
         </div>
 
@@ -19,8 +18,8 @@ const Services = () => {
           <img src="images/icon-2.png" alt="" />
           <h3>rent house</h3>
           <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Doloremque, incidunt.
+            Find the perfect rental property with ease and personalized options
+            for you.
           </p>
         </div>
 
@@ -28,36 +27,30 @@ const Services = () => {
           <img src="images/icon-3.png" alt="" />
           <h3>sell house</h3>
           <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Doloremque, incidunt.
+            Maximize your property's value with our professional selling
+            services.
           </p>
         </div>
 
         <div className="box">
           <img src="images/icon-4.png" alt="" />
           <h3>flats and buildings</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Doloremque, incidunt.
-          </p>
+          <p>Explore premium flats and buildings designed for modern living.</p>
         </div>
 
         <div className="box">
           <img src="images/icon-5.png" alt="" />
           <h3>shops and malls</h3>
           <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Doloremque, incidunt.
+            Secure prime retail spaces in vibrant and bustling commercial
+            locations.
           </p>
         </div>
 
         <div className="box">
           <img src="images/icon-6.png" alt="" />
           <h3>24/7 service</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Doloremque, incidunt.
-          </p>
+          <p>Experience round-the-clock support for all your property needs.</p>
         </div>
       </div>
     </section>
