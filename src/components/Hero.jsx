b6899f7c1b8413afd2e33640ba9735b952eb5d53
Sardar-Ali-Murad <mar389786@gmail.com>
2024-08-08 05:25:00 -0700

    changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,64 +1,76 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
 
-const Hero = () => {
+export default function App() {
   return (
-    <section className="home" id="home">
-      <div className="swiper home-slider">
-        <div className="swiper-wrapper">
-          <section
-            className="swiper-slide slide"
-            style={{ background: "url(images/home-slide-1.jpg) no-repeat" }}
-          >
-            <div className="content">
-              <h3>we provide best service</h3>
-              <p>
-                Experience unparalleled excellence in property management and
-                real estate solutions tailored to your needs!
-              </p>
-              <a href="#about" className="btn">
-                get started
-              </a>
-            </div>
-          </section>
-
-          <section
-            className="swiper-slide slide"
-            style={{ background: "url(images/home-slide-2.jpg) no-repeat" }}
-          >
-            <div className="content">
-              <h3>making dream come to life</h3>
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Aspernatur beatae iusto pariatur laborum magnam eos!
-              </p>
-              <a href="#about" className="btn">
-                get started
-              </a>
-            </div>
-          </section>
-
-          <section
-            className="swiper-slide slide"
-            style={{ background: "url(images/home-slide-3.jpg) no-repeat" }}
-          >
-            <div className="content">
-              <h3>from concept to creation</h3>
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Aspernatur beatae iusto pariatur laborum magnam eos!
-              </p>
-              <a href="#about" className="btn">
-                get started
-              </a>
-            </div>
-          </section>
+    <>
+      <div className="home" id="home">
+        <div className="swiper home-slider">
+          <div className="swiper-wrapper">
+            <Swiper navigation={true} modules={[Navigation]}>
+              <SwiperSlide>
+                <div
+                  className="slide"
+                  style={{
+                    background: "url(images/home-slide-1.jpg) no-repeat",
+                  }}
+                >
+                  <div className="content home-section">
+                    <h3>we provide best service</h3>
+                    <p>
+                      Experience unparalleled excellence in property management
+                      and real estate solutions tailored to your needs!
+                    </p>
+                    <a href="#about" className="btn">
+                      get started
+                    </a>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div
+                  className="swiper-slide slide"
+                  style={{
+                    background: "url(images/home-slide-2.jpg) no-repeat",
+                  }}
+                >
+                  <div className="content home-section">
+                    <h3>making dream come to life</h3>
+                    <p>
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                      Aspernatur beatae iusto pariatur laborum magnam eos!
+                    </p>
+                    <a href="#about" className="btn">
+                      get started
+                    </a>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div
+                  className="swiper-slide slide"
+                  style={{
+                    background: "url(images/home-slide-3.jpg) no-repeat",
+                  }}
+                >
+                  <div className="content home-section">
+                    <h3>from concept to creation</h3>
+                    <p>
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                      Aspernatur beatae iusto pariatur laborum magnam eos!
+                    </p>
+                    <a href="#about" className="btn">
+                      get started
+                    </a>
+                  </div>
+                </div>
+              </SwiperSlide>
+            </Swiper>
+          </div>
         </div>
-
-        <div className="swiper-button-next"></div>
-        <div className="swiper-button-prev"></div>
       </div>
-    </section>
+    </>
   );
-};
-
-export default Hero;
+}
