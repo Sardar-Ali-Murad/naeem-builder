28d9e9c2f4c14139302c9f70467e2d689b4c36d8
Sardar-Ali-Murad <mar389786@gmail.com>
2024-08-11 11:48:20 -0700

    removing the font-owsome links

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,5 @@ import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import "font-awesome/css/font-awesome.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
